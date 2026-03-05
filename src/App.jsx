3b77f5bb1939e94753f3adbeb4362515c30de040
hemanth5055 <kanalahemanthrdy@gmail.com>
2026-03-05 14:34:54 +0530

    Standardized frontend routes

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,19 @@ import PageNotFound from "components/commons/PageNotFound";
 import Product from "components/Product/Index";
 import ProductList from "components/ProductList/Index";
 import { Route, Switch, Redirect } from "react-router-dom";
+import routes from "routes";
 
-const App = () => (
-  <Switch>
-    <Route exact component={ProductList} path="/products" />
-    <Route exact component={Product} path="/products/:slug" />
-    <Redirect exact from="/" to="/products" />
-    <Route component={PageNotFound} path="*" />
-  </Switch>
-);
+const App = () => {
+  const { show, index } = routes.products;
+
+  return (
+    <Switch>
+      <Route exact component={ProductList} path={index} />
+      <Route exact component={Product} path={show} />
+      <Redirect exact from="/" to="/products" />
+      <Route component={PageNotFound} path="*" />
+    </Switch>
+  );
+};
 
 export default App;
--- src/components/ProductList/ProductListItem.jsx
+++ src/components/ProductList/ProductListItem.jsx
@@ -1,17 +1,23 @@
 import { Typography } from "neetoui";
 import { Link } from "react-router-dom";
+import routes from "routes";
+import { buildUrl } from "utils/url";
 
-const ProductListItem = ({ imageUrl, name, offerPrice, slug }) => (
-  <Link
-    className="neeto-ui-border-black neeto-ui-rounded-xl flex w-48 flex-col items-center justify-between border p-4"
-    to={`products/${slug}`}
-  >
-    <img alt={name} className="h-40 w-40" src={imageUrl} />
-    <Typography className="text-center" weight="semibold">
-      {name}
-    </Typography>
-    <Typography>${offerPrice}</Typography>
-  </Link>
-);
+const ProductListItem = ({ imageUrl, name, offerPrice, slug }) => {
+  const { show } = routes.products;
+
+  return (
+    <Link
+      className="neeto-ui-border-black neeto-ui-rounded-xl flex w-48 flex-col items-center justify-between border p-4"
+      to={buildUrl(show, { slug })}
+    >
+      <img alt={name} className="h-40 w-40" src={imageUrl} />
+      <Typography className="text-center" weight="semibold">
+        {name}
+      </Typography>
+      <Typography>${offerPrice}</Typography>
+    </Link>
+  );
+};
 
 export default ProductListItem;
