f145e438438225993a87cd098b863966d88ec527
hemanth5055 <kanalahemanthrdy@gmail.com>
2026-03-07 19:24:07 +0530

    Added pagination to product listing page

--- src/components/ProductList/Index.jsx
+++ src/components/ProductList/Index.jsx
@@ -4,17 +4,22 @@ import { Header, PageLoader } from "components/commons";
 import { useFetchProducts } from "hooks/reactQuery/useProductsApi";
 import useDebounce from "hooks/useDebounce";
 import { Search } from "neetoicons";
-import { Input, NoData } from "neetoui";
+import { Input, NoData, Pagination } from "neetoui";
 import { isEmpty } from "ramda";
 
+import { DEFAULT_PAGE_INDEX, DEFAULT_PAGE_SIZE } from "./constants";
 import ProductListItem from "./ProductListItem";
 
 const ProductList = () => {
   const [searchKey, setSearchKey] = useState("");
+  const [currentPage, setCurrentPage] = useState(DEFAULT_PAGE_INDEX);
   const debouncedSearchKey = useDebounce(searchKey);
-  const { data: { products = [] } = {}, isLoading } = useFetchProducts({
-    searchTerm: debouncedSearchKey,
-  });
+  const { data: { products = [], totalProductsCount } = {}, isLoading } =
+    useFetchProducts({
+      searchTerm: debouncedSearchKey,
+      page: currentPage,
+      pageSize: DEFAULT_PAGE_SIZE,
+    });
 
   // const fetchProducts = async () => {
   //   try {
@@ -48,7 +53,10 @@ const ProductList = () => {
             prefix={<Search />}
             type="search"
             value={searchKey}
-            onChange={event => setSearchKey(event.target.value)}
+            onChange={event => {
+              setSearchKey(event.target.value);
+              setCurrentPage(DEFAULT_PAGE_INDEX);
+            }}
           />
         }
       />
@@ -61,6 +69,14 @@ const ProductList = () => {
           ))}
         </div>
       )}
+      <div className="mb-5 self-end">
+        <Pagination
+          count={totalProductsCount}
+          navigate={page => setCurrentPage(page)}
+          pageNo={currentPage || DEFAULT_PAGE_INDEX}
+          pageSize={DEFAULT_PAGE_SIZE}
+        />
+      </div>
     </div>
   );
 };
